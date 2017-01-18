1b102367b74d57d41d9846a64559b6f34a955117
Erick Boyzo <erickb59@gmail.com>
2017-01-17 22:30:52 -0500

    fix routes issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
 	resources :posts
-  get 'posts#new' => 'posts#new'
+  #get 'posts#new' => 'posts#new'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
